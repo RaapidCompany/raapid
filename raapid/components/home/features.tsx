a24a6100cbfbea0bd284db9bc697a33f74d15496
raapidcompany <raapidcompany@gmail.com>
2025-07-01 12:59:37 +0100

    Delete raapid directory

--- raapid/components/home/features.tsx
+++
@@ -1,9 +0,0 @@
-import { Card, CardHeader, CardDescription, CardContent} from "@/components/ui/card"
-
-export default function Features() {
-    return(
-        <section id="features" className="font-inter">
-            <Card></Card>
-        </section>
-    )
-}
