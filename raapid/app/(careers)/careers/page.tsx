c13cbed33e92656431678a76b44c34e29aeb3614
Pleasant Yomi <110311997+pleasantyomi@users.noreply.github.com>
2025-07-17 10:31:24 +0100

    Update page.tsx

--- raapid/app/(careers)/careers/page.tsx
+++ raapid/app/(careers)/careers/page.tsx
@@ -1,8 +1,10 @@
 import Container from "@/components/careers/container"
+import Hero from "@/components/careers/hero"
 
 export default function Page() {
     return(
         <div>
+            <Hero />
             <Container />
         </div>
     )
